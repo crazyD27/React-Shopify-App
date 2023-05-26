75f5227238269a5e42ef2ff2e2c0630d61a68e7d
Roger-Davis <rd3328425@gmail.com>
2023-05-26 17:51:04 +0530

    Changes

--- src/components/pages/CampaignOver.jsx
+++ src/components/pages/CampaignOver.jsx
@@ -50,7 +50,7 @@ const CampaignOver = () => {
         })
         axios.get(API.BASE_URL + 'user/id/',{
             headers: {
-                Authorization: `Token ${token}`
+                Authorization: `Token ${localStorage.getItem('Token')}`
         }})
         .then(function (response) {
             console.log("User ID", response);
